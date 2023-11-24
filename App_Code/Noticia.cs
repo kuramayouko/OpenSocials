dd6ac6dda6fa9ae565c1fd367dbf305b67a799f0
vicoelho <82888644+vicoelho@users.noreply.github.com>
2023-11-24 19:43:04 -0300

    Add files via upload

---
+++ App_Code/Noticia.cs
@@ -0,0 +1,22 @@
+﻿namespace OpenSocials.App_Code
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Sqlite;
+    using System.Collections.Generic;
+
+    public class Noticia
+    {
+        public int id { get; set; }
+        public string tittle { get; set; }
+        public string text { get; set; }
+        public int media_id { get; set; }
+        public int is_approved { get; set; }
+        public int fb_postid { get; set; }
+        public int insta_postid { get; set; }
+        public string date_created { get; set; }
+        public string date_posted { get; set; }
+        public string date_schedule { get; set; }
+    }
+    public DbSet<AppConfig> AppConfigs { get; set; }
+}
---
+++ App_Code/Usuario.cs
@@ -0,0 +1,20 @@
+﻿namespace OpenSocials.App_Code
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Sqlite;
+    using System.Collections.Generic;
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string nickname { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public int attempts { get; set; }
+        public int is_admin { get; set; }
+        public int is_commenter { get; set; }
+        public int is_reviewer { get; set; }
+    }
+    public DbSet<AppConfig> AppConfigs { get; set; }
+}
